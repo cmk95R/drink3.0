aaddcd4ebe945d93408c4b6ca815b5285067953a
Roberto <barraganrobertocarlos4@gmail.com>
2025-05-29 13:57:26 -0300

    Aplicando ciertas limpiezas de jean

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,31 +3,12 @@ import { login, register, profile, showRegisterForm, showLoginForm , logout, res
 
 import carrito from "../controllers/carrito.controller.js"
 import path from 'path';
-import { fileURLToPath } from 'url';
-//import {authRequired}  from "../middlewares/auth.middleware.js";
-import { validacionesRegister } from "../validations/validacionesRegister.js";
-import { validacionesLogin } from "../validations/validacionesLogin.js";
-
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
+
 const router = Router();
 
-
-
-// Ruta para mostrar la pagin de login 
-//router.get('/login', (req, res) => {
-//    res.render('login'); // Renderiza la vista login.ejs
-//});
-
-//router.post('/login', validacionesLogin, login);
-//Ruta para mostrar la pagina de carrito
-
-//router.get('/profile', authRequired, profile,(req, res) => {
-//    res.sendFile(path.join(__dirname, '../pages/profile.html'));
-//});
 
 //Ruta para mostrar la pagina de carrito
 router.get('/carrito', authenticate, carrito.carrito,(req, res) => {
@@ -35,44 +16,6 @@ router.get('/carrito', authenticate, carrito.carrito,(req, res) => {
 });
 
 
-router.get('/medios-pago', (req, res) => {
-   res.sendFile(path.join(__dirname, '../pages/medios-pago.html'));
-});
-
-
-// Ruta para mostrar la pagina de confirmacion de Registro
-//router.get('/confirm', (req, res) => {
-//  res.sendFile(path.join(__dirname, '../pages/confirm.html'));
-//});
-
-// Ruta para mostrar la pagina de Cambio de Contraseña
-
-
-//router.get('/sistema-notificaciones', (req, res) => {
-//    res.sendFile(path.join(__dirname, '../pages/sistema-notificaciones.html'));
-//});
-
-
-
-//Ruta para procesar el registro 
-//router.get("/register",  showRegisterForm); // Muestra el formulario sin datos
-
-//Ruta para procesar el registro 
-//router.post('/register', validacionesRegister,register);
-
-
-//Ruta para cambiar la pass del usuario 
-
-
-
-
-// Otras rutas 
-
-//router.post('/login', validacionesLogin, login);
-//router.post('/register', validacionesRegister, register);
-//router.post('/change-password', authRequired, changePassword);
-//router.get("/logout", logout);
-//router.get("/profile", authRequired, profile);
 
 
 //router.get("/logout", logout)
@@ -90,19 +33,7 @@ router.post('/change-password', authenticate, changePassword);
 router.get('/logout', logout);
 
 
-//router.get('/register', (req, res)=>{
-//    res.render('register', {
-//        formData: {}, // para evitar error en el ejs
-//        errors: []    // lo mismo para errores
-//      });
-//    });
-//router.get('/register', showRegisterForm);
-//router.get('/login', (req, res)=>{
-  //  res.render('login');
-//});
-//router.get('/login', showLoginForm);
 
 
-////
 
 export default router;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import controller from '../controllers/indexController.js';  // Asegúrate de importar correctamente
 import { detailProduct } from '../controllers/detailProduct.js';
-//import Product from '../models/product.js';
+
 const router = express.Router();
 
-//router.post('/', abrirModalCarrito);
+
 
 router.get('/', controller.index);  // Usa el método correcto del controlador
 router.get('/search', controller.index);  // Puedes mantener esta ruta si es necesaria
